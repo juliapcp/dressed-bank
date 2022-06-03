27791c40066840b0afc923294491065be0f55446
juliapcp <juliapcp2003@gmail.com>
2022-06-03 15:12:04 -0300

    Adicionado acesso à variáveis globais da seção através do navbar

--- app/Views/includes/navbar.php
+++ app/Views/includes/navbar.php
@@ -7,7 +7,7 @@
             <i class="fa fa-user fa-fw w3-xxxlarge"></i>
         </div>
         <div class="w3-col s8 w3-bar">
-            <span>Bem vindo(a), <strong>--</strong></span><br>
+            <span>Bem vindo(a), <strong><?php echo $_SESSION['nome'] ?></strong></span><br>
             <button href="#" onclick="abrirMeuPerfil()" class="open-button w3-bar-item w3-button" id="abrirPopup">Visualizar
                 perfil</button>
             <button href="#" onclick="fecharMeuPerfil()" class="open-button w3-bar-item w3-button" id="fecharPopup" style="visibility: hidden; position: absolute;">Fechar perfil</button>
@@ -16,25 +16,25 @@
     </div>
     <div class="form-popup" id="meuPerfil" style="display: none; border-left: 12px solid #029913;">
         <b style=" margin-left: 20px;">Nome:</b><br>
-        <p style=" margin-left: 20px;">--</p>
-        <b style=" margin-left: 20px;">Email:</b>
-        <p style=" margin-left: 20px;">--</p>
+        <p style=" margin-left: 20px;"><?php echo $_SESSION['nome'] ?></p>
+        <b style=" margin-left: 20px;">Username:</b>
+        <p style=" margin-left: 20px;"><?php echo $_SESSION['username'] ?></p>
     </div>
     <hr>
     <div class="w3-bar-block">
         <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i>  Fechar Menu</a>
-        <a href="/" class="w3-bar-item w3-button w3-padding"><i class="fa fa-eye fa-fw"></i>  Dashboard</a>
-        <a href="/transacoes/cadastrar" class="w3-bar-item w3-button w3-padding"><i class="fa fa-file-text-o fa-fw"></i>  Extrato</a>
+        <a href="/dashboard" class="w3-bar-item w3-button w3-padding"><i class="fa fa-eye fa-fw"></i>  Dashboard</a>
+        <a href="" class="w3-bar-item w3-button w3-padding"><i class="fa fa-file-text-o fa-fw"></i>  Extrato</a>
         <div class="w3-col s8 w3-bar">
             <button href="#" onclick="abrirPoupanca()" class="open-button w3-bar-item w3-button" id="abrirPonpup"><i class="fa fa-bank fa-fw"></i>  Poupança</button>
         </div>
         <div class="form-popup" id="minhaPoupanca" style="display: none;">
             <b class="w3-button w3-padding" style=" margin-left: 20px;"><i class="fa fa-bank fa-fw"></i>  Saldo -----</b><br>
-            <b style=" margin-left: 20px;"><a href="/usuario/aplicacao" class="w3-button w3-padding"><i class="fa fa-bank fa-fw"></i>  Aplicação</a></b><br>
-            <b style=" margin-left: 20px;"><a href="/conta/resgate" class="w3-button w3-padding"><i class="fa fa-bank fa-fw"></i>  Resgate</a></a></b>
+            <b style=" margin-left: 20px;"><a href="/usuario/aplicacao/<?php echo $_SESSION['idUsuario'] ?>" class="w3-button w3-padding"><i class="fa fa-bank fa-fw"></i>  Aplicação</a></b><br>
+            <b style=" margin-left: 20px;"><a href="/conta/resgate/<?php echo $_SESSION['idUsuario'] ?>" class="w3-button w3-padding"><i class="fa fa-bank fa-fw"></i>  Resgate</a></a></b>
         </div>
-        <a href="/transacoes/cadastrar" class="w3-bar-item w3-button w3-padding"><i class="fa fa-money fa-fw"></i>  Pagamento</a>
-        <a href="/transacoes/cadastrar" class="w3-bar-item w3-button w3-padding"><i class="fa fa-exchange fa-fw"></i>  Transferência</a>
+        <a href="/transacao/cadastro" class="w3-bar-item w3-button w3-padding"><i class="fa fa-money fa-fw"></i>  Pagamento</a>
+        <a href="/transacao/transferencia" class="w3-bar-item w3-button w3-padding"><i class="fa fa-exchange fa-fw"></i>  Transferência</a>
     </div>
 </nav>
 
