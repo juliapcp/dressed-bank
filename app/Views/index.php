8ba39073ced4d9f353e68ef761dbc4aa7d3a0199
juliapcp <juliapcp2003@gmail.com>
2022-06-09 14:02:02 -0300

    Adicionado ícone na tabela de transações

--- app/Views/index.php
+++ app/Views/index.php
@@ -3,14 +3,10 @@
 <?php echo view('includes/head') ?>
 <?php echo view('includes/navbar') ?>
 <?php echo view('includes/footer') ?>
-
-
 <div class="w3-main" style="margin-left:300px;margin-top:43px;">
     <header class="w3-container" style="padding-top:22px">
         <h4 id="titulo">Resumo da conta corrente</h4>
-
     </header>
-
     <div class="w3-row-padding w3-margin-bottom">
         <div class="w3-third">
             <div class="w3-container w3-teal w3-padding-16">
@@ -42,20 +38,14 @@
                 <h4>Saldo</h4>
             </div>
         </div>
-
-
     </div>
-
-
     <div class="w3-container">
         <h4 id="titulo">Transações</h4>
         <table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white">
             <?php
             if ($transacoes != null) {
                 foreach ($transacoes as $transacao) {
-                    echo "<tr>
-                <td></td>
-                <td>" . $transacao['tipoTransacao'] . "</td>
+                    echo "<tr><td><i class=\"fa fa-". ($transacao['tipoTransacao'] == 'C' ? "plus w3-text-green" : "minus w3-text-red") ."\"></i></td>
                 <td>" . $transacao['datatransacao'] . "</td>
                 <td>" . $transacao['metodopagamento'] . "</td>
                 <td>" . $transacao['valor'] . "</td>
@@ -68,9 +58,6 @@
         </table>
     </div>
     <hr>
-
-
 </div>
 </body>
-
 </html>
