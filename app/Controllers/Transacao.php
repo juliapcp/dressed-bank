50f6b72c4154cba68dfa6b6932c06f2a23b0e527
juliapcp <juliapcp2003@gmail.com>
2022-06-10 14:08:19 -0300

    Adicionado saldo da poupança no navbar das operações

--- app/Controllers/Transacao.php
+++ app/Controllers/Transacao.php
@@ -8,8 +8,8 @@ use App\Models\ContaModel;
 
 class Transacao extends BaseController
 {
-    
-    public function mostraResgate($idUsuario = null, $validation = null )
+
+	public function mostraResgate($idUsuario = null, $validation = null)
 	{
 		if ($idUsuario != null) {
 			$transacaoModel = new TransacaoModel();
@@ -22,7 +22,7 @@ class Transacao extends BaseController
 		}
 	}
 
-	public function mostraAplicacao($idUsuario = null, $validation = null )
+	public function mostraAplicacao($idUsuario = null, $validation = null)
 	{
 		if ($idUsuario != null) {
 			$transacaoModel = new TransacaoModel();
@@ -37,171 +37,176 @@ class Transacao extends BaseController
 
 	public function aplicacao()
 	{
-			$rules = [
-				
-				'valor' => 'required'
-			];
-			if ($this->validate($rules)) {
-                $transacaoModel = new TransacaoModel();
-                $contaModel = new ContaModel();
-				$data = array(
-					'tipo' => 'D',
-					'valor' => $this->request->getVar('valor'),
-					'conta' => ($contaModel->getContaUsuario($_SESSION['idUsuario'], 'C')[0]['idconta']),
-					'metodopagamento' => 'aplicação',
-					'datatransacao' => date("Y-m-d"),
-					'descricao' => 'Valor Aplicado' 
-				);
-                $data2 = array(
-					'tipo' => 'C',
-					'valor' => $this->request->getVar('valor'),
-					'conta' => ($contaModel->getContaUsuario($_SESSION['idUsuario'], 'P')[0]['idconta']),
-					'metodopagamento' => 'aplicação',
-					'datatransacao' => date("Y-m-d"),
-					'descricao' => 'Valor da aplicação' 
-				);
-				
-				if (($transacaoModel->getSaldo($_SESSION['idUsuario'], 'C') < $this->request->getVar('valor'))||( $this->request->getVar('valor') <= 0)) {
-					$this->session->setFlashdata('loginFail', 'SALDO MERDA.');
-					return redirect()->to(base_url('transacao/aplicacao'));
-				} else {
+		$rules = [
+
+			'valor' => 'required'
+		];
+		if ($this->validate($rules)) {
+			$transacaoModel = new TransacaoModel();
+			$contaModel = new ContaModel();
+			$data = array(
+				'tipo' => 'D',
+				'valor' => $this->request->getVar('valor'),
+				'conta' => ($contaModel->getContaUsuario($_SESSION['idUsuario'], 'C')[0]['idconta']),
+				'metodopagamento' => 'aplicação',
+				'datatransacao' => date("Y-m-d"),
+				'descricao' => 'Valor Aplicado'
+			);
+			$data2 = array(
+				'tipo' => 'C',
+				'valor' => $this->request->getVar('valor'),
+				'conta' => ($contaModel->getContaUsuario($_SESSION['idUsuario'], 'P')[0]['idconta']),
+				'metodopagamento' => 'aplicação',
+				'datatransacao' => date("Y-m-d"),
+				'descricao' => 'Valor da aplicação'
+			);
+
+			if (($transacaoModel->getSaldo($_SESSION['idUsuario'], 'C') < $this->request->getVar('valor')) || ($this->request->getVar('valor') <= 0)) {
+				$this->session->setFlashdata('loginFail', 'SALDO MERDA.');
+				return redirect()->to(base_url('transacao/aplicacao'));
+			} else {
 				$transacaoModel->insereTransacao($data);
 				$transacaoModel->insereTransacao($data2);
 				return redirect()->to(base_url('/dashboard'));
-				}
-			} else {
-				$this->mostraResgate($_SESSION['idUsuario'], $this->validator);
 			}
+		} else {
+			$this->mostraResgate($_SESSION['idUsuario'], $this->validator);
+		}
 	}
 
 	//n sei se ta certo 
 	public function resgate()
 	{
-			$rules = [
-				
-				'valor' => 'required'
-			];
-			if ($this->validate($rules)) {
-                $transacaoModel = new TransacaoModel();
-                $contaModel = new ContaModel();
-				$data = array(
-					'tipo' => 'C',
-					'valor' => $this->request->getVar('valor'),
-					'conta' => ($contaModel->getContaUsuario($_SESSION['idUsuario'], 'C')[0]['idconta']),
-					'metodopagamento' => 'resgate',
-					'datatransacao' => date("Y-m-d"),
-					'descricao' => 'Valor resgatado' 
-				);
-                $data2 = array(
-					'tipo' => 'D',
-					'valor' => $this->request->getVar('valor'),
-					'conta' => ($contaModel->getContaUsuario($_SESSION['idUsuario'], 'P')[0]['idconta']),
-					'metodopagamento' => 'resgate',
-					'datatransacao' => date("Y-m-d"),
-					'descricao' => 'Valor do resgate' 
-				);
-				
-				if (($transacaoModel->getSaldo($_SESSION['idUsuario'], 'P') < $this->request->getVar('valor'))||( $this->request->getVar('valor') <= 0)) {
-					$this->session->setFlashdata('loginFail', 'Saldo insuficiente para efetuar o resgate!!');
-					return redirect()->to(base_url('/transacao/resgate'));
-				} else {
+		$rules = [
+
+			'valor' => 'required'
+		];
+		if ($this->validate($rules)) {
+			$transacaoModel = new TransacaoModel();
+			$contaModel = new ContaModel();
+			$data = array(
+				'tipo' => 'C',
+				'valor' => $this->request->getVar('valor'),
+				'conta' => ($contaModel->getContaUsuario($_SESSION['idUsuario'], 'C')[0]['idconta']),
+				'metodopagamento' => 'resgate',
+				'datatransacao' => date("Y-m-d"),
+				'descricao' => 'Valor resgatado'
+			);
+			$data2 = array(
+				'tipo' => 'D',
+				'valor' => $this->request->getVar('valor'),
+				'conta' => ($contaModel->getContaUsuario($_SESSION['idUsuario'], 'P')[0]['idconta']),
+				'metodopagamento' => 'resgate',
+				'datatransacao' => date("Y-m-d"),
+				'descricao' => 'Valor do resgate'
+			);
+
+			if (($transacaoModel->getSaldo($_SESSION['idUsuario'], 'P') < $this->request->getVar('valor')) || ($this->request->getVar('valor') <= 0)) {
+				$this->session->setFlashdata('loginFail', 'Saldo insuficiente para efetuar o resgate!!');
+				return redirect()->to(base_url('/transacao/resgate'));
+			} else {
 				$transacaoModel->insereTransacao($data);
 				$transacaoModel->insereTransacao($data2);
 				return redirect()->to(base_url('/dashboard'));
-				}
-			} else {
-				$this->mostraResgate($_SESSION['idUsuario'], $this->validator);
 			}
+		} else {
+			$this->mostraResgate($_SESSION['idUsuario'], $this->validator);
+		}
 	}
-	
-	public function extrato($idUsuario) {
+
+	public function extrato($idUsuario)
+	{
 		$extrato = new TransacaoModel();
-        $data["extratos"] = $extrato->extrato($idUsuario);
-		
+		$data["extratos"] = $extrato->extrato($idUsuario);
+
 		return view('/usuario/extrato', $data);
 	}
 
-    public function mostraPagamento()
-    {
-        return view('transacao/pagamento');
-    }
-    public function cadastraPagamento()
-    {
-        $rules = [
-            'metodopagamento' => 'required',
-            'valor' => 'required',
-            'datatransacao' => 'required'
-        ];
+	public function mostraPagamento()
+	{
+		$transacao = new TransacaoModel();
+		$data["saldoPoupança"] = $transacao->getSaldo($_SESSION['idUsuario'], 'P');
+		return view('transacao/pagamento', $data);
+	}
+	public function cadastraPagamento()
+	{
+		$rules = [
+			'metodopagamento' => 'required',
+			'valor' => 'required',
+			'datatransacao' => 'required'
+		];
 
-        $contaModel = new ContaModel();
-        $conta = $contaModel->getContaUsuario($_SESSION['idUsuario'], "C")[0]["idconta"];
-        $transacaoModel = new TransacaoModel();
+		$contaModel = new ContaModel();
+		$conta = $contaModel->getContaUsuario($_SESSION['idUsuario'], "C")[0]["idconta"];
+		$transacaoModel = new TransacaoModel();
 
-        if ($this->validate($rules)) {
-            $data = array(
-                'metodopagamento' => $this->request->getVar('metodopagamento'),
-                'valor' => $this->request->getVar('valor'),
-                'descricao' => $this->request->getVar('descricao'),
-                'tipo' => "D",
-                'conta' => $conta,
-                'datatransacao' => $this->request->getVar('datatransacao')
-            );
-            $transacaoModel->insereTransacao($data);
-            return redirect()->to(base_url('/dashboard'));
-        }    }
-    public function mostraTransferencia()
-    {
-        return view('transacao/transferencia');
-    }
-    public function cadastraTransferencia()
-    {
-        $rules = [
-            'valor' => 'required',
-            'datatransacao' => 'required'
-        ];
+		if ($this->validate($rules)) {
+			$data = array(
+				'metodopagamento' => $this->request->getVar('metodopagamento'),
+				'valor' => $this->request->getVar('valor'),
+				'descricao' => $this->request->getVar('descricao'),
+				'tipo' => "D",
+				'conta' => $conta,
+				'datatransacao' => $this->request->getVar('datatransacao')
+			);
+			$transacaoModel->insereTransacao($data);
+			return redirect()->to(base_url('/dashboard'));
+		}
+	}
+	public function mostraTransferencia()
+	{
+		$transacao = new TransacaoModel();
+		$data["saldoPoupança"] = $transacao->getSaldo($_SESSION['idUsuario'], 'P');
+		return view('transacao/transferencia', $data);
+	}
+	public function cadastraTransferencia()
+	{
+		$rules = [
+			'valor' => 'required',
+			'datatransacao' => 'required'
+		];
 
-        $contaModel = new ContaModel();
+		$contaModel = new ContaModel();
 		$conta = $contaModel->getContaUsuario($_SESSION['idUsuario'], "C")[0]["idconta"];
 		$contaPoupanca = $contaModel->getContaUsuario($_SESSION['idUsuario'], "P")[0]["idconta"];
-        $transacaoModel = new TransacaoModel();
+		$transacaoModel = new TransacaoModel();
 		$contaDestino = $contaModel->getDados($this->request->getVar('contadestino'));
-		if($contaDestino != null){
-			if($contaDestino["id"] != $conta && $contaDestino["id"] != $contaPoupanca){
-        if ($this->validate($rules)) {
-			if($transacaoModel->getSaldo($_SESSION['idUsuario'], 'C') >= $this->request->getVar('valor') ){
-            $data = array(
-                'metodopagamento' => "transferencia",
-                'valor' => $this->request->getVar('valor'),
-                'descricao' => $this->request->getVar('descricao') . "| Conta de destino: ". $contaDestino["numero"],
-                'tipo' => "D",
-                'conta' => $conta,
-                'datatransacao' => $this->request->getVar('datatransacao')
-            );
-            $transacaoModel->insereTransacao($data);
-            $data2 = array(
-                'metodopagamento' => "transferencia",
-                'valor' => $this->request->getVar('valor'),
-                'descricao' => $this->request->getVar('descricao') . "| Conta de origem: ". $contaModel->getContaUsuario($_SESSION['idUsuario'], "P")[0]["numero"],
-                'tipo' => "C",
-                'conta' => $contaDestino["id"],
-                'datatransacao' => $this->request->getVar('datatransacao')
-            );
-			$transacaoModel = new TransacaoModel();
-            $transacaoModel->insereTransacao($data2);
-            return redirect()->to(base_url('/dashboard'));
-		} else {
+		if ($contaDestino != null) {
+			if ($contaDestino["id"] != $conta && $contaDestino["id"] != $contaPoupanca) {
+				if ($this->validate($rules)) {
+					if ($transacaoModel->getSaldo($_SESSION['idUsuario'], 'C') >= $this->request->getVar('valor')) {
+						$data = array(
+							'metodopagamento' => "transferencia",
+							'valor' => $this->request->getVar('valor'),
+							'descricao' => $this->request->getVar('descricao') . "| Conta de destino: " . $contaDestino["numero"],
+							'tipo' => "D",
+							'conta' => $conta,
+							'datatransacao' => $this->request->getVar('datatransacao')
+						);
+						$transacaoModel->insereTransacao($data);
+						$data2 = array(
+							'metodopagamento' => "transferencia",
+							'valor' => $this->request->getVar('valor'),
+							'descricao' => $this->request->getVar('descricao') . "| Conta de origem: " . $contaModel->getContaUsuario($_SESSION['idUsuario'], "P")[0]["numero"],
+							'tipo' => "C",
+							'conta' => $contaDestino["id"],
+							'datatransacao' => $this->request->getVar('datatransacao')
+						);
+						$transacaoModel = new TransacaoModel();
+						$transacaoModel->insereTransacao($data2);
+						return redirect()->to(base_url('/dashboard'));
+					} else {
 						$this->session->setFlashdata('mensagem', 'Você não possui saldo suficiente para fazer essa transferência.');
 						return redirect()->to(base_url('/transacao/transferencia'));
-		}
-        }
-		
-	} else {
+					}
+				}
+			} else {
 				$this->session->setFlashdata('mensagem', 'A conta destino não pode ser sua conta.');
 				return redirect()->to(base_url('/transacao/transferencia'));
-	}
-		    } else {
-				$this->session->setFlashdata('mensagem', 'A conta destino não existe, tente novamente.');
-				return redirect()->to(base_url('/transacao/transferencia'));
 			}
-		    }
+		} else {
+			$this->session->setFlashdata('mensagem', 'A conta destino não existe, tente novamente.');
+			return redirect()->to(base_url('/transacao/transferencia'));
+		}
+	}
 }
